99d497e71fc6dc8d816e38f513cb96d0f6043143
Prathik Raj <prathikraj@ymail.com>
2016-04-29 12:26:59 +0530

    Loads a random tech blog in a browser

---
+++ random-tech-blog.py
@@ -0,0 +1,53 @@
+import requests
+import xmltodict
+import random
+import webbrowser
+
+
+class Feed():
+
+    def __init__(self, url):
+        self.url = url
+
+    def getUrl(self):
+        return self.url
+
+
+class FeedList():
+
+    def __init__(self, feeds):
+        self.feeds = feeds
+
+    def getFeeds(self):
+        return self.feeds
+
+
+class LoadFeed():
+
+    def __init__(self, url):
+        page = requests.get(url)
+        feedList = []
+        feeds = xmltodict.parse(page.text)
+        for f in feeds['opml']['body']['outline']['outline']:
+            feedList.append(Feed(f['@htmlUrl']))
+
+        self.feedList = FeedList(feedList)
+
+    def getFeedList(self):
+        return self.feedList
+
+
+class LoadRandom():
+
+    def __init__(self, url):
+        fl = LoadFeed(url)
+        self.feedList = fl.getFeedList().getFeeds()
+
+    def openTechBlogInBrowser(self):
+        webbrowser.open(self.randomTechBlog())
+
+    def randomTechBlog(self):
+        return random.choice(self.feedList).getUrl()
+
+lr = LoadRandom("https://raw.githubusercontent.com/kilimchoi/engineering-blogs/master/engineering_blogs.opml")
+print lr.openTechBlogInBrowser()
